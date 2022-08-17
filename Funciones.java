0618aae56f03955a232bf55cb28262e5e0e7f307
Gonzalo Minvielle <gonzalominvielle24@gmail.com>
2022-08-16 22:32:41 -0300

    Se agregan ejercicios de Funciones, tipos de cambio.

---
+++ Funciones.java
@@ -0,0 +1,7 @@
+run:
+Ingrese la cantidad de Euros que desea cambiar: 
+100
+A que moneda desea cambiar?: 1- Libras. 2- Dolares. 3- Yenes. 
+2
+ahora usted tiene 128.61100000000002 dolares.
+BUILD SUCCESSFUL (total time: 26 seconds)
